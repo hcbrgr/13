0569422137ca00e2b289456283a3669c072f7d30
WDelenclos <wdelenclos@gmail.com>
2018-01-10 10:13:33 +0100

    Fix

--- app/code/local/13/Dbchange/sql/13_dbchange_setup/mysql4-install-0.1.0.php
+++ app/code/local/13/Dbchange/sql/13_dbchange_setup/mysql4-install-0.1.0.php
@@ -5,7 +5,7 @@ $installer->startSetup();
 
 $attribute  = array(
     'group'                     => 'General',
-        'input'                     => 'text',
+        'input'                     => 'select',
         'type'                      => 'int',
         'label'                     => 'Createur',
         'source'                    => 'eav/entity_attribute_source_boolean',
@@ -13,18 +13,18 @@ $attribute  = array(
         'visible'                   => 1,
         'required'                  => 1,
         'visible_on_front'          => 1,
-        'is_html_allowed_on_front'  => 0,
+        'is_html_allowed_on_front'  => 1,
         'is_configurable'           => 0,
         'searchable'                => 0,
-        'filterable'                => 1,
+        'filterable'                => 0,
         'comparable'                => 0,
         'unique'                    => false,
         'user_defined'              => false,
-        'default'                   => '0',
+        'default'           => '0',
         'is_user_defined'           => false,
         'used_in_product_listing'   => true
 );
-$installer->addAttribute('catalog_category', 'Createur', $attribute);
+$installer->addAttribute('catalog_category', 'createur', $attribute);
 $installer->endSetup();
 
 ?>
